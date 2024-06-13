12fdbfe5a84e4551331bf3fb506aa7e8a279ca83
shashank <shashank.vinfotech@gmail.com>
2024-06-13 20:12:48 +0530

    Changes in app.java to remove errors

--- src/test/java/ShashankSharma/AppTest.java
+++ src/test/java/ShashankSharma/AppTest.java
@@ -1,8 +1,8 @@
 package ShashankSharma;
 
-import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertTrue;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
